8e2a84f6d1ea826eef310823bd7bb8373649f4e5
Nelly <nelly.babillon@gmail.com>
2025-04-22 18:42:14 +0200

    Ajout de la structure de la page d'accueil avec des vidéos et des sections pour les podcasts

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,26 +3,43 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About</h1>
+<div>
+  <div class="title-container" style="background-color: black">
+      <div class="anim-title">
 
-	<p>
-		LA SUCRERIE est un laboratoire de création de contenus dont le territoire s’étend de la création
-		vidéo dans toutes ses formes — du clip au brand content et du documentaire à la fiction — au
-		podcast et à la photographie. La Sucrerie a à cœur de représenter la société, le contemporain et
-		d’explorer de nouvelles histoires et de nouveaux horizons avec un œil neuf et une ambition
-		créative. Incarnée par ses talents, La Sucrerie façonne des solutions sur-mesure à ses
-		partenaires à qui elle propose des formules conjuguant autant de disciplines qu’il en faut pour
-		faire passer l’image et l’histoire de ses partenaires au niveau suivant.
-	</p>
+        <div class="end-anim">
+          <h3 class="end-small-title">About</h3>
+
+          <div class="end-text"><p><strong>﻿LA SUCRERIE</strong> est un laboratoire de création de contenus dont le territoire s’étend de la création vidéo dans toutes ses formes —&nbsp;du clip au brand content et du documentaire à la fiction — au podcast et à la photographie. La Sucrerie a à cœur de représenter la société, le contemporain et d’explorer de nouvelles histoires et de nouveaux horizons avec un œil neuf et une ambition créative. Incarnée par ses talents, La Sucrerie façonne des solutions sur-mesure à ses partenaires à qui elle propose des formules conjuguant autant de disciplines qu’il en faut pour faire passer l’image et l’histoire de ses partenaires au niveau suivant.</p><p><br></p><p><br></p><p><br></p></div>
+        </div>
+    </div>
+  </div>
 </div>
-
-<div class="gallery-wrapper">
-	<img src="/images/bureau-01.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-02.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-03.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-04.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-05.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-06.jpg" alt="Logo Sucrerie" />
-	<img src="/images/bureau-01.jpg" alt="Logo Sucrerie" />
+<div class="grid">
+  <div id="justified-gallery" class="justified-gallery" style="height: 2029.4px;">
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 220px; height: 320.72px; top: 0px; left: 0px;">
+          <img width="200" src="images/bureau-01.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 491px; height: 320.72px; top: 0px; left: 220px;">
+          <img width="200" src="images/bureaux-02.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 711px; height: 450px; top: 320.72px; left: 0px;">
+          <img width="200" src="images/bureaux-03.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 711px; height: 450px; top: 770.72px; left: 0px;">
+          <img width="200" src="images/bureaux-04.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry">
+          <img width="200" src="images/bureaux-05.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 711px; height: 450px; top: 1220.72px; left: 0px;">
+          <img width="200" src="images/bureaux-06.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry">
+          <img width="200" src="images/bureaux-07.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+      <div id="img-gallerie" class="jg-entry jg-entry-visible" style="width: 711px; height: 358.678px; top: 1670.72px; left: 0px;">
+          <img width="200" src="images/bureaux-08.jpg" style="width: 220px; height: 321px; margin-left: -110px; margin-top: -160.5px;" alt="bureau-01">
+      </div>
+  </div>
 </div>
--- src/routes/podcasts/+page.svelte
+++ src/routes/podcasts/+page.svelte
@@ -3,43 +3,71 @@
 	<meta name="description" content="Podcasts" />
 </svelte:head>
 
-<div class="show-podcast">
-	<img class="img-podcast" src="/images/podcast-01.jpg" alt="couverture"/>
-	<h3 id="title-banner">Zen Zen</h3>
-	<div class="desc-talent podcast">
-		<p>
-			Zen Zen est un podcast Original Spotify qui propose des séances de méditation guidées par des
-			artistes de la scène francophone. La chanteuse et influenceuse Chloé Stafler a écrit des
-			textes sur mesure que ces voix familières viennent magnifier pour un moment de calme et de
-			sérénité.
-		</p>
+<div class="jouuue-div">
+	<img
+		class="jouuue-logo"
+		src="/assets/LOWWOOD/PNG/Logo-LW-blanc-aa27800da99415b95750e757b2d9fb34f929c07673224053f961c070bd906885.png"
+	/>
+</div>
+<div id="fullmosaicpodcast">
+	<div class="video-container-mosaic">
+		<a href="/podcasts/7">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/9sq6uf9o9tsbtgpctvl00gmsx8pz"
+			/>
+		</a>
 	</div>
-	<br />
-	<div class="podcast-audios">
-		<div class="d-flex justify-content-center align-items-center mt-3">
-			<h4 class="sub-title-banner bande-annonce">Bande annonce</h4>
-			<audio
-				autoplay="autoplay"
-				controls="controls"
-				src="https://res.cloudinary.com/lasucrerie/video/upload/26x6cdixuv3ak4a8cgsbq2fb4ytf"
-			></audio>
-		</div>
-		<h4 class="sub-title-banner mt-0">
-			<a
-				target="_blank"
-				href="https://open.spotify.com/show/1bCshCNNesT2KqZ2Y0U1DL?si=D5uPyk  GTT9KK4T6nWZId1g"
-			>
-				<p>Écouter le podcast ici</p>
-			</a>
-		</h4>
+	<div class="video-container-mosaic">
+		<a href="/podcasts/1">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/338r3h6xn2l1t5wr5qikkxkw83c3"
+			/>
+		</a>
 	</div>
-	<div class="desc-talent podcast">
-		<p>Zen Zen est un podcast Original Spotify produit en association avec La Sucrerie</p>
-		<p>Présenté et imaginé par Chloé Stafler</p>
-		<p>Produit par Steven Clerima et Anne Rivoallan</p>
-		<p>Réalisé et monté : Tatiana Benhamou et Laura Laarman</p>
-		<p>Mixé par Dimitri Ben Hamou</p>
-		<p>La musique du générique a été composée par Alex Deeping</p>
-		<p>Musiques additionnelles : MYMA - My Music Agency</p>
+	<div class="video-container-mosaic">
+		<a href="/podcasts/2">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/q207i2cvyie6qfiy8skbklmpjm0x"
+			/>
+		</a>
+	</div>
+	<div class="video-container-mosaic">
+		<a href="/podcasts/3">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/rhvqgaggvtr87sofehuum07gzkpt"
+			/>
+		</a>
+	</div>
+	<div class="video-container-mosaic">
+		<a href="/podcasts/4">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/z55e6m0ho5ypxuv9ekhd6cte2hb4"
+			/>
+		</a>
+	</div>
+	<div class="video-container-mosaic">
+		<a href="/podcasts/5">
+			<img
+				style="max-width: 100%;"
+				class="img-mansory"
+				data-color="red"
+				src="https://res.cloudinary.com/lasucrerie/image/upload/cj3t28v3nd0zjzvub2r7adc6v4g3"
+			/>
+		</a>
 	</div>
 </div>
